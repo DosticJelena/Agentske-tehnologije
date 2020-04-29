05c79a04d913777d5f57d33c6aa503a7adfcf53c
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-29 22:26:01 +0200

    promenjeno u UserMessage
    bez setovanja propertija, salje se ceo objekat umesto tekstualne poruke

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MessageManagerRemote.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MessageManagerRemote.java
@@ -4,10 +4,12 @@ import javax.ejb.Remote;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 
+import models.UserMessage;
+
 @Remote
 public interface MessageManagerRemote {
 	
-	public void post(AgentMessage msg);
+	public void post(UserMessage msg);
 	public Session getSession();
 	public MessageConsumer getConsumer();
 	
