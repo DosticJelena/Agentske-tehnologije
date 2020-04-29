70c83d1d98b1bfffb09bbb057544e694ebcac3fa
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-29 21:47:10 +0200

    anotiran topic na koji se smestaju poruke

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MDBConsumer.java
@@ -1,11 +1,16 @@
 package messagemanager;
 
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
 import agents.AgentListRemote;
 
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/topic/publicTopic") })
 public class MDBConsumer implements MessageListener {
 
 	@EJB
