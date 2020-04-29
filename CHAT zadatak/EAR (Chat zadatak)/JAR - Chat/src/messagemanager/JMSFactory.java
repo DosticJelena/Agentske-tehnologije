8a3b08dd682901c07eb05787165fe67141527a22
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-29 21:36:24 +0200

    kreiran JMSFactory
    - dodata polja connection factory i connection
    - dodat topic (publicTopic)

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/JMSFactory.java
@@ -0,0 +1,20 @@
+package messagemanager;
+
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Topic;
+
+@Singleton
+@LocalBean
+public class JMSFactory {
+
+	private Connection connection;
+	@Resource(lookup = "java:jboss/exported/jms/RemoteConnectionFactory")
+	private ConnectionFactory connectionFactory;
+	@Resource(lookup = "java:jboss/exported/jms/topic/publicTopic")
+	private Topic defaultTopic;
+	
+}
