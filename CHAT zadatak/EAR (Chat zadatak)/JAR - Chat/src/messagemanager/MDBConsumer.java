6bb66515a00684ca0f666fa7defcbf77a2f4de5d
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-29 21:46:11 +0200

    dodato polje agents (AgentListRemote)

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/MDBConsumer.java
@@ -1,10 +1,16 @@
 package messagemanager;
 
+import javax.ejb.EJB;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+import agents.AgentListRemote;
+
 public class MDBConsumer implements MessageListener {
 
+	@EJB
+	private AgentListRemote agents;
+	
 	public MDBConsumer() {
 
 	}
