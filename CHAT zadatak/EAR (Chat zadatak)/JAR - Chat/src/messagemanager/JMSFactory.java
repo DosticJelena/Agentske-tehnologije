3b761020a1416b61115039607eddc0af88a9b644
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-29 21:37:39 +0200

    dodate metode
    - uzimanje sesije
    - uzimanje producera (topic)
    - uzimanje consumera (topic)

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/JMSFactory.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messagemanager/JMSFactory.java
@@ -5,6 +5,11 @@ import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.QueueSession;
+import javax.jms.Session;
 import javax.jms.Topic;
 
 @Singleton
@@ -17,4 +22,27 @@ public class JMSFactory {
 	@Resource(lookup = "java:jboss/exported/jms/topic/publicTopic")
 	private Topic defaultTopic;
 	
+	public Session getSession() {
+		try {
+			return connection.createSession(false, QueueSession.AUTO_ACKNOWLEDGE);
+		} catch (JMSException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
+	public MessageProducer getProducer(Session session) {
+		try {
+			return session.createProducer(defaultTopic);
+		} catch (JMSException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
+	public MessageConsumer getConsumer(Session session) {
+		try {
+			return session.createConsumer(defaultTopic);
+		} catch (JMSException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
 }
