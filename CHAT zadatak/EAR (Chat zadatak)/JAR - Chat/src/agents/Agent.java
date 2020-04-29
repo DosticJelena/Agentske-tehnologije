d1bd5e013ee52ac4349eb10ec52c7a81c965f60c
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-04-30 00:31:01 +0200

    changed return type

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/Agent.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/Agent.java
@@ -7,5 +7,5 @@ import javax.jms.Message;
 public interface Agent extends Serializable {
 	public String init();
 	public void handleMessage(Message message);
-	public String getAgentId();
+	public long getAgentId();
 }
