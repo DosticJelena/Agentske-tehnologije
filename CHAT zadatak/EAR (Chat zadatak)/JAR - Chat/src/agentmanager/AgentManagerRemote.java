b5bc5110492d6b707bea0a296c63252095ff2b72
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 19:39:26 +0200

    created agent manager interface

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agentmanager/AgentManagerRemote.java
@@ -0,0 +1,13 @@
+package agentmanager;
+
+import javax.ejb.Remote;
+
+import agents.Agent;
+
+@Remote
+public interface AgentManagerRemote {
+
+	public String startAgent(String name);
+	public Agent getAgentById(String agentId);
+	
+}
