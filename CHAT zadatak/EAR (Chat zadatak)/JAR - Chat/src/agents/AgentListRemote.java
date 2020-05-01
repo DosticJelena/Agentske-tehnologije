e2f5f6c54408be0ba80fe8701d8767938bd3ed00
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 19:32:43 +0200

    created AgentList interface

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/AgentListRemote.java
@@ -0,0 +1,10 @@
+package agents;
+
+import java.util.HashMap;
+
+public interface AgentListRemote {
+
+	public HashMap<String, Agent> getRunningAgents();
+	public void addRunningAgent(String key, Agent agent);
+	
+}
