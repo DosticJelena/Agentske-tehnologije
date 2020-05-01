bf3b71a2cf594961f8fbff09a9e188e384387e34
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:42:49 +0200

    onMessage method implementation
    - finds receiver from object property
    - find Agent based on receiver property (host or chat agent)
    - then host/chat agent handle received message

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
@@ -3,9 +3,11 @@ package messaging;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+import agents.Agent;
 import agents.AgentListRemote;
 
 @MessageDriven(activationConfig = {
@@ -21,9 +23,15 @@ public class MDBConsumer implements MessageListener {
 	}
 	
 	@Override
-	public void onMessage(Message arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onMessage(Message message) {
+		String receiver;
+		try {
+			receiver = (String) message.getObjectProperty("receiver");
+			Agent agent = (Agent) agents.getRunningAgents().get(receiver);
+			agent.handleMessage(message);
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
