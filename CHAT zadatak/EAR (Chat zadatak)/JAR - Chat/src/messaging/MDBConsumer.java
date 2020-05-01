575b99fc035b65c820d446e1ef5e37b3e3d036ed
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:38:31 +0200

    annotation MessageDriven
    - "subscribing" to "publicTopic" topic

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
@@ -1,8 +1,13 @@
 package messaging;
 
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/topic/publicTopic") })
 public class MDBConsumer implements MessageListener {
 
 	@Override
