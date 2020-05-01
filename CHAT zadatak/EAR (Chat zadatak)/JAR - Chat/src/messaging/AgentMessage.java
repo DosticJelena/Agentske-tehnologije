e32e649f38e10a0ce2a5e6352f392ad832e2d711
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:09:24 +0200

    created default AgentMessage class
    - message factory will post AgentMessage
    - MDBConsumer will get that AgentMessage

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/AgentMessage.java
@@ -0,0 +1,15 @@
+package messaging;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgentMessage implements Serializable {
+
+	private static final long serialVersionUID = 4750922547689000321L;
+	public Map<String, Serializable> userArgs;
+	
+	public AgentMessage() {
+		userArgs = new HashMap<>();
+	}
+}
