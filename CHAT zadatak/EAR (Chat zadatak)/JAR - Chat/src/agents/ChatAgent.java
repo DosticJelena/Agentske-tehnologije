a8c47bdbdb539f1284e788f9a57e492a4f3d0190
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 14:16:32 +0200

    created two implementations for Agent interface
    - chat agent for messaging
    - host agent for managing users

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/ChatAgent.java
@@ -0,0 +1,25 @@
+package agents;
+
+import javax.jms.Message;
+
+public class ChatAgent implements Agent {
+
+	@Override
+	public String init() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void handleMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getAgentId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/HostAgent.java
@@ -0,0 +1,25 @@
+package agents;
+
+import javax.jms.Message;
+
+public class HostAgent implements Agent {
+
+	@Override
+	public String init() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void handleMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getAgentId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
