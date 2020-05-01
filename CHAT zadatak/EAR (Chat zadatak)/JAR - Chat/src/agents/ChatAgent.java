5d594597470ad1b968461838a0e35793463a4f97
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 19:37:36 +0200

    injected agent list

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/ChatAgent.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/agents/ChatAgent.java
@@ -1,13 +1,19 @@
 package agents;
 
+import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateful;
 import javax.jms.Message;
 
-@Stateful
+//@Stateful
 @Remote(Agent.class)
 public class ChatAgent implements Agent {
 
+	@EJB
+	private AgentListRemote agents;
+	
+	private String agentId;
+	
 	@Override
 	public String init() {
 		// TODO Auto-generated method stub
