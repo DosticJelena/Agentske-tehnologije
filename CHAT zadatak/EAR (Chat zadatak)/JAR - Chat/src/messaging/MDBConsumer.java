02636f371003b1b272fbd3635dced0e00127a083
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:40:18 +0200

    injected AgentList and added constructor

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
@@ -1,15 +1,25 @@
 package messaging;
 
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+
+import agents.AgentListRemote;
 
 @MessageDriven(activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
 		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/topic/publicTopic") })
 public class MDBConsumer implements MessageListener {
 
+	@EJB
+	private AgentListRemote agents;
+	
+	public MDBConsumer() {
+		
+	}
+	
 	@Override
 	public void onMessage(Message arg0) {
 		// TODO Auto-generated method stub
