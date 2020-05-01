200d16536bb0086b88cafbb2c4a840aabf7c8e22
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 13:44:14 +0200

    rest endpoints interface

---
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/rest/RestBeanRemote.java
@@ -0,0 +1,30 @@
+package rest;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+import javax.ws.rs.core.Response;
+
+import org.jboss.security.auth.spi.Users.User;
+
+import models.UserMessage;
+
+@Remote
+public interface RestBeanRemote {
+
+	public Response login(User user);
+
+	public Response register(User user);
+
+	public List<User> getLoggedInUsers();
+	
+	public Response logOutUser(long userId);
+	
+	public List<User> getRegisteredUsers();
+	
+	public List<UserMessage> sendMessageToAllUsers(UserMessage msg);
+	
+	public UserMessage sendMessageToUser(long userId, UserMessage msg);
+	
+	public List<UserMessage> getAllUserMessages(long userId);
+}
