87a68a128b0d4c68fb271d09b60f2ef953b7a59c
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:04:14 +0200

    create connection in post construct method

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/JMSFactory.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/JMSFactory.java
@@ -1,10 +1,12 @@
 package messaging;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.Topic;
 
 @Singleton
@@ -17,4 +19,13 @@ public class JMSFactory {
 	@Resource(lookup = "java:jboss/exported/jms/topic/publicTopic")
 	private Topic defaultTopic;
 	
+	@PostConstruct
+	public void postConstruction() {
+		try {
+			connection = connectionFactory.createConnection("guest", "guest.guest.1");
+			connection.start();
+		} catch (JMSException ex) {
+			throw new IllegalStateException(ex);
+		} 
+	}
 }
