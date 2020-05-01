04c23bdde28029afc546c439705e7f04de8910e5
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-01 20:35:27 +0200

    implementing MessageListener interface

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/messaging/MDBConsumer.java
@@ -1,5 +1,14 @@
 package messaging;
 
-public class MDBConsumer {
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class MDBConsumer implements MessageListener {
+
+	@Override
+	public void onMessage(Message arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
