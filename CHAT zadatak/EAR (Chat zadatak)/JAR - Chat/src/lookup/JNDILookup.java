6a9d38990ea173357a7c4262cd96805befcf53c1
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-03 01:46:51 +0200

    created method for creating initial context with added properties

--- CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/lookup/JNDILookup.java
+++ CHAT zadatak/EAR (Chat zadatak)/JAR - Chat/src/lookup/JNDILookup.java
@@ -1,6 +1,6 @@
 package lookup;
 
-import java.util.Map;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -16,7 +16,7 @@ import messaging.MessageManagerRemote;
 
 public class JNDILookup {
 
-	public static final String JNDIPathChat = "ejb:/EAR - Chat/JAR_-_Chat//";
+	public static final String JNDIPathChat = "ejb:/EAR - Chat/JAR_-_Chat/";
 	public static final String AgentManagerLookup = JNDIPathChat + AgentManager.class.getSimpleName() + "!"
 			+ AgentManagerRemote.class.getName();
 	public static final String MessageManagerLookup = JNDIPathChat + MessageManager.class.getSimpleName() + "!"
@@ -26,17 +26,27 @@ public class JNDILookup {
 	public static final String HostAgentLookup = JNDIPathChat + HostAgent.class.getSimpleName() + "!"
 			+ Agent.class.getName() + "?stateful";
 
+	private static Context createInitialContext() throws NamingException {
+        Properties jndiProperties = new Properties();
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, 
+          "org.jboss.naming.remote.client.InitialContextFactory");
+        jndiProperties.put(Context.URL_PKG_PREFIXES, 
+          "org.jboss.ejb.client.naming");
+        jndiProperties.put(Context.PROVIDER_URL, 
+           "http-remoting://localhost:8080");
+        jndiProperties.put("jboss.naming.client.ejb.context", true);
+        return new InitialContext(jndiProperties);
+    }
+	
 	public static <T> T lookUp(String name, Class<T> c) {
-		Map<String, String> env = System.getenv();
-		env.forEach((k, v) -> System.out.println(k + ":" + v));
 		
 		T bean = null;
 		try {
-			Context context = new InitialContext();
-
+			Context context = createInitialContext();
+			
 			System.out.println("Looking up: " + name);
 			bean = (T) context.lookup(name);
-
+			
 			context.close();
 
 		} catch (NamingException e) {
